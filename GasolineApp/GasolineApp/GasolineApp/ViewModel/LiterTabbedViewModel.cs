d0f9b547b6a4aea5eb7e4d34551f14fa414616c1
marijanpezo <marijanpezo@yahoo.com>
2017-01-17 21:13:21 +0100

    showing something

--- GasolineApp/GasolineApp/GasolineApp/ViewModel/LiterTabbedViewModel.cs
+++ GasolineApp/GasolineApp/GasolineApp/ViewModel/LiterTabbedViewModel.cs
@@ -8,25 +8,42 @@ namespace GasolineApp.ViewModel
 {
     public class LiterTabbedViewModel: ViewModelBase
     {
-        public List<Refill> refills { get; private set; }
+        public List<Refill> refills { get; private set; } = new List<Refill>();
         //public RefillDatabase refillDatabase;
-        
+
+        private decimal sumLiter;
+        public decimal SumLiter
+        {
+            get { return sumLiter; }
+            set { Set(ref sumLiter, value); }
+        }
+
         public LiterTabbedViewModel()
         {
-            if (refills == null)
-            {
-                refills = new List<Refill>();
-            }
+//            if (refills == null)
+//            {
+//                refills = new List<Refill>();
+//            }
+//            else
+//            {
+//                
+//            }
+
+            //            refillDatabase = new RefillDatabase();
+            //            var refillsDatabse = refillDatabase.GetRefills();
+            //
+            //            refills = (List<Refill>) refillsDatabse;
             
-//            refillDatabase = new RefillDatabase();
-//            var refillsDatabse = refillDatabase.GetRefills();
-//
-//            refills = (List<Refill>) refillsDatabse;
 
             Messenger.Default.Register<Refill>(this, (refillSend) =>
             {
                 refills.Add(refillSend);
+                foreach (var litre in refills)
+                {
+                    sumLiter += litre.Litres;
+                }
             });
+
 
             
         }
